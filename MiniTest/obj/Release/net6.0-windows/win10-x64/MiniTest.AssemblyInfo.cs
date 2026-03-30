851ceddffc067ea46addbbfb1f3ecd02a8962ec4
2nd <faustin60@gmail.com>
2026-03-30 09:57:07 +0300

    Сделана сборка

--- MiniTest/obj/Release/net6.0-windows/win10-x64/MiniTest.AssemblyInfo.cs
+++ MiniTest/obj/Release/net6.0-windows/win10-x64/MiniTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc629c7f328bee68e4c6fd6f3287a6ab21bfa463")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
